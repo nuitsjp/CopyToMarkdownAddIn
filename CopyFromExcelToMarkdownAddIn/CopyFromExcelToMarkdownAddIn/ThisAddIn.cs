b713c32a7ee5df4dd82a78130b1468384b4849b2
nuits.jp <nuits.jp@live.jp>
2016-10-20 11:54:47 +0900

    Add Project.

---
+++ CopyFromExcelToMarkdownAddIn/CopyFromExcelToMarkdownAddIn/ThisAddIn.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+
+namespace CopyFromExcelToMarkdownAddIn
+{
+    public partial class ThisAddIn
+    {
+        /// <summary>
+        /// Minimum required select the number of rows.
+        /// </summary>
+        private const int MinRowCount = 3;
+        /// <summary>
+        /// Alignment Left
+        /// </summary>
+        private const int AlignmentLeft = 1;
+        /// <summary>
+        /// Alignment Center
+        /// </summary>
+        private const int AlignmentCenter = -4108;
+        /// <summary>
+        /// Alignment Right
+        /// </summary>
+        private const int AlignmentRight = -4152;
+
+        /// <summary>
+        /// Button in ContextMenu.
+        /// </summary>
+        private CommandBarButton _copyToMarkdownButton;
+        /// <summary>
+        /// Startup event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+            // Create button in ContextMenu
+            var menuItem = MsoControlType.msoControlButton;
+            _copyToMarkdownButton = (CommandBarButton)Application.CommandBars["Cell"].Controls.Add(menuItem, missing, missing, 1, true);
+            _copyToMarkdownButton.Style = MsoButtonStyle.msoButtonCaption;
+            _copyToMarkdownButton.Caption = "Copy to Markdown";
+            _copyToMarkdownButton.Tag = "0";
+            _copyToMarkdownButton.Click += CopyToMarkdown;
+        }
+
+        /// <summary>
+        /// Shutdown event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// CopyToMarkdown
+        /// </summary>
+        /// <param name="ctrl"></param>
+        /// <param name="cancelDefault"></param>
+        private void CopyToMarkdown(CommandBarButton ctrl, ref bool cancelDefault)
+        {
+            var range = Application.Selection as Range;
+            if (range != null)
+            {
+                var rowsCount = range.Rows.Count;
+                if (MinRowCount <= rowsCount)
+                {
+                    var columnsCount = range.Count / rowsCount;
+                    var resultBuffer = new StringBuilder();
+                    var separatorBuffer = new StringBuilder();
+                    for (int x = 1; x <= columnsCount; x++)
+                    {
+                        var cell = (Range)range.Cells[1, x];
+
+                        resultBuffer.Append("|");
+                        resultBuffer.Append(cell.Text == null ? string.Empty : cell.Text);
+                        switch ((int)cell.HorizontalAlignment)
+                        {
+                            case AlignmentLeft:
+                                separatorBuffer.Append("|-");
+                                break;
+                            case AlignmentCenter:
+                                separatorBuffer.Append("|:-:");
+                                break;
+                            case AlignmentRight:
+                                separatorBuffer.Append("|-:");
+                                break;
+                            default:
+                                separatorBuffer.Append("|-");
+                                break;
+                        }
+                    }
+                    // Partition of the header and data lines.
+                    // Process only after the first line.
+                    resultBuffer.Append("|");
+                    resultBuffer.Append(Environment.NewLine);
+                    separatorBuffer.Append("|");
+                    separatorBuffer.Append(Environment.NewLine);
+                    resultBuffer.Append(separatorBuffer);
+
+                    for (int y = 2; y <= rowsCount; y++)
+                    {
+                        for (int x = 1; x <= columnsCount; x++)
+                        {
+                            var cell = (Range)range.Cells[y, x];
+
+                            resultBuffer.Append("|");
+                            resultBuffer.Append(cell.Text == null ? string.Empty : cell.Text);
+                        }
+                        resultBuffer.Append("|");
+                        resultBuffer.Append(Environment.NewLine);
+                    }
+                    Clipboard.SetText(resultBuffer.ToString());
+                }
+                else
+                {
+                    MessageBox.Show(Properties.Resources.UnselectedErrorMessage);
+                }
+            }
+            else
+            {
+                MessageBox.Show(Properties.Resources.UnselectedErrorMessage);
+            }
+        }
+
+
+        #region VSTO で生成されたコード
+
+        /// <summary>
+        /// デザイナーのサポートに必要なメソッドです。
+        /// このメソッドの内容をコード エディターで変更しないでください。
+        /// </summary>
+        private void InternalStartup()
+        {
+            Startup += ThisAddIn_Startup;
+            Shutdown += ThisAddIn_Shutdown;
+        }
+        
+        #endregion
+    }
+}
