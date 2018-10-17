3378e7de0b9987a5362de27db613b13b3726fc32
nuits.jp <nuits.jp@live.jp>
2018-10-17 22:48:38 +0900

    Update release version.

--- CopyFromExcelToMarkdownAddIn/CopyFromExcelToMarkdownAddIn/Properties/AssemblyInfo.cs
+++ CopyFromExcelToMarkdownAddIn/CopyFromExcelToMarkdownAddIn/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、以下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます。
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
 
